cfa6ffb89e54f53f5b185e217ca7a7cff64a57bf
lordaris <audio.aris@gmail.com>
2022-12-05 11:20:09 -0600

    commit

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -5,8 +5,8 @@ const siteMetadata = {
   description: '',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteUrl: 'https://arisdev.vercel.app',
+  siteRepo: 'https://github.com/lordaris/my-portfolio',
   siteLogo: '/static/images/logo.svg',
   image: '/static/images/avatar.svg',
   socialBanner: '/static/images/twitter-card.png',
